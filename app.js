38fdf1af7dd89e8025d2051ec63d03dfde36c773
Rose Lalk <rose.lalk@kvk.nl>
2023-08-25 11:29:26 +0200

    Added a logging function that logs details about the request on every new request and added a .gitignore file.

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(myLogger);
+
+function myLogger (req, res, next) {
+  console.log("Request IP: " + req.ip);
+  console.log("Request Method: " + req.method);
+  console.log("Request date: " + new Date());
+
+  next(); // THIS IS IMPORTANT!
+};
+
 
 app.get("/", (request, result) => {
-    result.send("Hello World! I love cheese.")
-})
+  result.send("Hello World! I love cheese.");
+});
 app.get("/about", (request, result) => {
-    result.send("This is the about page")
-})
+  result.send("This is the about page");
+});
 app.get("/contact", (request, result) => {
-    result.send("Contact page")
-})
+  result.send("Contact page");
+});
 
 app.use((request, result, next) => {
-    result.status(404).send("Oops! Something went wrong")
-})
+  result.status(404).send("Oops! Something went wrong");
+});
 
 app.listen(port, () => {
-    console.log(`Exercise app listening on port ${port}`);
-})
+  console.log(`Exercise app listening on port ${port}`);
+});
